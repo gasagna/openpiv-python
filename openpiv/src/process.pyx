0ac2ebf1475e63b839858e9ddf122a00080450e3
Davide Lasagna <lasagnadavide@gmail.com>
2011-07-14 23:51:08 +0200

    Added licence info and __all__ variable

--- openpiv/src/process.pyx
+++ openpiv/src/process.pyx
@@ -1,5 +1,25 @@
 """This module is dedicated to advanced algorithms for PIV image analysis."""
 
+__licence__ = """
+Copyright (C) 2011  www.openpiv.net
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__all__ = ['extended_search_area_piv', 'get_coordinates', 'get_field_shape',
+           'correlate_windows', 'normalize_intensity', 'CorrelationFunction']
+
 import numpy as np
 import numpy.ma as ma
 from numpy.fft import rfft2,irfft2,fftshift
